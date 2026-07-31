5f1fd529a482e9f44e5acf676b6430fbe4921eb6
JUSOLI-0609 <sori7700@gmail.com>
2026-07-31 14:31:05 +0900

    콘솔 매니저
    함수 선언 및 정의 완

--- Text RPG/Console_Manager.h
+++ Text RPG/Console_Manager.h
@@ -6,21 +6,24 @@ class Console_Manager
 //콘솔 화면 기능을 모아둔 클래스 
 {
 private:
-    int _width;
-    int _height;
+    int _Width;
+    int _Height;
 //콘솔의 가로, 세로 크기 저장
 
 
 public:
     Console_Manager();
     //생성자
+
     Console_Manager(int width, int height);
+    //객체 선언
 
     void Set_Console_Size();
+    //콘솔 창 크기 설정
+
     void Set_Cursor_Position(int x, int y);
     //커서 위치 이동 함수
-    //windows에서 꺼내온 함수라 네이밍 수정 불가
-
+   
     void Clear();
-    //콘솔화면 지우는 함수
+    //화면 초기화 함수
 };
